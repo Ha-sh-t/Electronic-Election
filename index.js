6c1a86f13fcd0920bf7e069ce9b40b9331b5a496
harshit <777harshitkumar@gmail.com>
2025-01-23 18:17:47 +0530

    create https server

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 import express from 'express';
-
+import { readFileSync } from 'fs';
+import https from 'https';
+import * as path from 'path';
 const app = express();
+const options = {
+    key:readFileSync(path.join('CA' , 'server-key.pem')),
+    cert:readFileSync(path.join('CA','server-cert.pem'))
+}
+const ssl_server =  https.createServer(options , app)
 
 app.get('/' , (req , res)=>{
     res.send("Welcome to the Electronic Election App!")
 })
+app.use('/',(req , res)=>{
+    res.send("Welcome");
+})
 
-app.listen(3000 , ()=>{
-    console.log("server is listening on port 3000 ...")
+ssl_server.listen(3000 , ()=>{
+    console.log("Your secure server is running on  port 3000")
 })
