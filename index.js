e9bd9c2ba94190654e75da5a171674384a739e4c
harshit <777harshitkumar@gmail.com>
2025-01-26 19:37:50 +0530

    add jwtauth stage 1

--- index.js
+++ index.js
@@ -3,14 +3,22 @@ import { readFileSync } from 'fs';
 import https from 'https';
 import * as path from 'path';
 import connectToMongoDB from './config/mongodb-config.js';
-const app = express(); //responsible for request and response
+import userRouter from './src/user/user.routes.js';
 
-//key and signed certificate
+
+const app = express(); //responsible for request and response
+app.use(express.json());
+
+
+//key and signed certificate--------------------------------
 const options = {
     key:readFileSync(path.join('CA' , 'server-key.pem')),
     cert:readFileSync(path.join('CA','server-cert.pem'))
 }
-const ssl_server =  https.createServer(options , app) //acting as CA(certificate authority)
+const ssl_server =  https.createServer(options , app) //acting as CA(certificate authority)----------
+
+
+app.use('/api/user' , userRouter);
 
 app.get('/' , (req , res)=>{
     res.send("Welcome to the Electronic Election App!")
